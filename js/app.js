11683668b5ffae3da5f53336bba8e2cfa16d5d53
Junior Felix <juniorfelix8825@outlook.com>
2020-10-13 10:46:36 +0300

    Adds transition animations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Smooth Scroll
 const menuLinks = document.querySelectorAll('.menu__link')
 function clickHandler(e) {
     e.preventDefault();
@@ -13,3 +14,33 @@ function clickHandler(e) {
 for (const link of menuLinks){
     link.addEventListener('click', clickHandler)
 }
+
+// Fade in 
+const faders = document.querySelectorAll('.fade-in')
+const slides = document.querySelectorAll(`.from`)
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+}
+const appearOnScroll = new IntersectionObserver(
+  function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        return
+      } else {
+        entry.target.classList.add('appear')
+        appearOnScroll.unobserve(entry.target)
+      }
+    })
+  },
+  appearOptions
+)
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader)
+})
+
+slides.forEach( slider => {
+  appearOnScroll.observe(slider)
+})
