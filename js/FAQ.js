1dd6488cd29f6fa29a4e8727062ad48119005cad
Junior Felix <juniorfelix8825@outlook.com>
2020-11-03 18:56:32 +0300

    Corrects FAQ page

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,23 +1,16 @@
 //accordions
 const accordionBtns = document.querySelectorAll(".accordion__button");
-let closed = true;
 const toggleActiveAccordion = (e) => {
     e = e || window.event;
     const btnClicked = e.target || e.srcElement;
     const accordion = btnClicked.parentNode;
     const panel = accordion.querySelector(".panel");
 
-    if (closed) {
-        accordion.classList.add("active");
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        // plus.innerHTML = "&minus;";
-        closed = false;
-    } else {
-        accordion.classList.remove("active");
+    accordion.classList.toggle("active");
+    if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
-        // const plus = accordion.querySelector(".accordion__add");
-        // plus.innerHTML = "&plus;";
-        closed = true;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
     }
 };
 
